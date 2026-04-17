c875a0037c3441b45f2dd889e7980435031ce478
AkhmadRoziqin <rzqn0513050a@gmail.com>
2026-04-17 10:56:15 +0800

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,15 @@
+# if
+# else
+# elif
+
+nama = input("masukkan nama")
+NIM = input("masukkan PIN NIM")
+
+if nama =="Roziqin" :
+    NIM =="25241071"
+    print("Hai saya Roziqin")
+    print("NIM saya 25241071")
+else : 
+    print("Mohon maaf anda Roziqin")
+    print("NIM anda tidak bisa diakses")    
+print("program selesai")
